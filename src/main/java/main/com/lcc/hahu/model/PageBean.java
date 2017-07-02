1eaac04b985232ec2737963e187f86dcb9708f6b
liangchengcheng <1038127753@qq.com>
2017-07-03 07:21:22 +0800

    add bean class file
    add bean class file

---
+++ src/main/java/main/com/lcc/hahu/model/PageBean.java
@@ -0,0 +1,53 @@
+package main.com.lcc.hahu.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by liangchengcheng on 2017/7/3.
+ */
+
+public class PageBean<T> {
+
+    private int allPage;
+    private int curPage;
+
+    private List<T> list = new ArrayList<T>();
+
+    public PageBean() {
+    }
+
+    public PageBean(int allPage, int curPage) {
+        this.allPage = allPage;
+        this.curPage = curPage;
+    }
+
+    public int getAllPage() {
+        return allPage;
+    }
+
+    public void setAllPage(int allPage) {
+        this.allPage = allPage;
+    }
+
+    public int getCurPage() {
+        return curPage;
+    }
+
+    public void setCurPage(int curPage) {
+        this.curPage = curPage;
+    }
+
+    public List<T> getList() {
+        return list;
+    }
+
+    public void setList(List<T> list) {
+        this.list = list;
+    }
+
+    @Override
+    public String toString() {
+        return "PageBean{" + "allPage=" + allPage + ", curPage=" + curPage + ", list=" + list + '}';
+    }
+}
